af75b202bdc353eaaf7322397602aee19bd08337
kshitij-404 <kshitij4ka@gmail.com>
2022-05-29 23:57:51 +0530

    add sections in landing page

--- src/screens/Home/homeScreen.jsx
+++ src/screens/Home/homeScreen.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import Navbar from "../../components/Navbar/navbar";
+import React from 'react'
+import './homeScreen.css'
+import Navbar from '../../components/Navbar/navbar'
 
 function HomeScreen() {
   return (
     <div>
       <Navbar />
-      Home
+      <div className="homepage-section">
+        <div>
+          <div className="homepage-headings">Heading</div>
+          <div className="homepage-text">Text</div>
+        </div>
+        <div className="homepage-image">
+          <img
+            alt="teaching"
+            src="https://res.cloudinary.com/dzerj4bzd/image/upload/v1653836937/AASF%20Website/homepage-teaching_aiiuv2.png"
+          ></img>
+        </div>
+      </div>
+      <div className="homepage-section">Home</div>
+      <div className="homepage-section">Home</div>
+      <div className="homepage-section">Home</div>
     </div>
-  );
+  )
 }
 
-export default HomeScreen;
+export default HomeScreen
