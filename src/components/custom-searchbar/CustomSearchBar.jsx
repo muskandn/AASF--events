527ddef3660c64be9d91a3d7e43674bea6dd7fd6
Akshith <akshithgandla7891@gmail.com>
2022-06-15 16:25:57 +0530

    change the classNames

--- src/components/custom-searchbar/CustomSearchBar.jsx
+++ src/components/custom-searchbar/CustomSearchBar.jsx
@@ -3,18 +3,18 @@ import "./customSearchbar.css";
 
 const CustomSearchBar = () => {
   return (
-    <div className="ab">
-      <div className="container">
-        <div className="search_wrap search_wrap_4">
-          <div className="search-box__tags">
-            <span className="ab">
+    <div className="SearchBar">
+      <div className="customSearchbarContainer">
+        <div className="customSearchbarContainer__search_wrap customSearchbarContainer__search_wrap_4">
+          <div className="customSearchbarContainer__searchboxTags">
+            <span className="SearchBar">
               <a href="#">Link_1</a>
               <a href="#">Link_2</a>
               <a href="#">Link_3</a>
             </span>
           </div>
-          <div className="search_box">
-            <div className="btn btn_common slide-right">
+          <div className="customSearchbarContainer__search_box">
+            <div className="customSearchbarContainer_btn">
               <i className="fas fa-search"></i>
             </div>
             <input type="text" className="input" placeholder="search..." />
